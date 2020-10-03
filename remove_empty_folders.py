b5537faaab8f698410cb155d5bd66de72a5f2343
trfunk <4313364+trfunk@users.noreply.github.com>
2020-10-03 16:48:06 +0200

    removed debug statement, removed unneeded loop

--- remove_empty_folders.py
+++ remove_empty_folders.py
@@ -1,3 +1,5 @@
+# Deletes empty folders recursively.
+# Takes a path as argument
 # Yoinked from https://stackoverflow.com/a/23488980
 
 import os
@@ -9,15 +11,13 @@ def remove_empty_dir(path):
     except OSError:
         pass
 
-def remove_empty_dirs(paths):
-    for path in paths:
-        for root, dirnames, filenames in os.walk(path, topdown=False):
-            for dirname in dirnames:
-                remove_empty_dir(os.path.realpath(os.path.join(root, dirname)))
+def remove_empty_dirs(path):
+    for root, dirnames, filenames in os.walk(path, topdown=False):
+        for dirname in dirnames:
+            remove_empty_dir(os.path.realpath(os.path.join(root, dirname)))
 
 if __name__ == "__main__":
     if sys.argv[1]:
-    	print(sys.argv[1])
-        #remove_empty_dirs(sys.argv[1])
+        remove_empty_dirs(sys.argv[1])
     else:
-        print("Please pass the paths to check as parameters to the script")
+        print("Please pass the path to check as parameter to the script")
